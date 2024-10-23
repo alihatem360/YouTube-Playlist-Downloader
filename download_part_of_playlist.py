3f6bd5ce114e8e98479907fc9db1e33aee849272
Ali Hatem <alihatemramadan5@gmail.com>
2024-10-23 18:41:22 +0300

    Refactor download script and update README

--- download_part_of_playlist.py
+++ download_part_of_playlist.py
@@ -2,7 +2,7 @@ from pytubefix import Playlist, exceptions
 import os
 
 
-def download_playlist(url):
+def download_part_of_playlist(url, start_index, end_index):
     try:
         yt_playlist = Playlist(url)
     except exceptions.PytubeError as e:
@@ -18,8 +18,8 @@ def download_playlist(url):
     if not os.path.exists(playlist_folder):
         os.makedirs(playlist_folder)
 
-    videos = yt_playlist.videos
-    for index, video in enumerate(videos, start=1):
+    videos = yt_playlist.videos[start_index-1:end_index]
+    for index, video in enumerate(videos, start=start_index):
         try:
             video.check_availability()
             stream = video.streams.filter(
@@ -33,13 +33,16 @@ def download_playlist(url):
         except Exception as e:
             print(f"An unexpected error occurred with {video.watch_url}: {e}")
 
-    print(f"The playlist has been downloaded successfully in {
+    print(f"The selected part of the playlist has been downloaded successfully in {
           playlist_folder}")
 
 
 if __name__ == "__main__":
     # طلب عنوان URL من المستخدم
     playlist_url = input("Enter the Youtube Playlist link: ")
+    start_index = int(
+        input("Enter the start index of the videos to download: "))
+    end_index = int(input("Enter the end index of the videos to download: "))
 
     # تنفيذ دالة التنزيل
-    download_playlist(playlist_url)
+    download_part_of_playlist(playlist_url, start_index, end_index)
--- youtube_one_video_downloader.py
+++ youtube_one_video_downloader.py
@@ -1,11 +1,41 @@
-from pytube import YouTube
+from pytubefix import YouTube, exceptions
+import os
 
-link = input("Enter the YouTube video link: ")
-DOWNLOAD_PATH = "./Download"  # specify your download path here
 
-yt = YouTube(link)
-stream = yt.streams.filter(
-    progressive=True, file_extension='mp4').get_highest_resolution()
-stream.download(DOWNLOAD_PATH)
+def download_video(url):
+    try:
+        yt_video = YouTube(url)
+    except exceptions.PytubeError as e:
+        print(f"Error initializing video: {e}")
+        return
 
-print("Downloaded: " + yt.title)
+    # الحصول على اسم الفيديو
+    video_title = yt_video.title
+
+    # إنشاء مجلد لحفظ الفيديو
+    video_folder = "Downloaded_Videos"
+    if not os.path.exists(video_folder):
+        os.makedirs(video_folder)
+
+    try:
+        yt_video.check_availability()
+        stream = yt_video.streams.filter(
+            progressive=True, file_extension='mp4').get_highest_resolution()
+        # Add the title to the video filename
+        filename = f"{video_title}.mp4"
+        stream.download(output_path=video_folder, filename=filename)
+        print(f"Downloaded: {filename}")
+    except exceptions.PytubeError as e:
+        print(f"Failed to download {yt_video.watch_url}: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred with {yt_video.watch_url}: {e}")
+
+    print(f"The video has been downloaded successfully in {video_folder}")
+
+
+if __name__ == "__main__":
+    # طلب عنوان URL من المستخدم
+    video_url = input("Enter the Youtube Video link: ")
+
+    # تنفيذ دالة التنزيل
+    download_video(video_url)
